338e15d1a24177f09ebb0164c0451ced43e6c73f
kevinquillen <kevin@inclind.com>
2013-05-23 19:34:44 -0700

    Start adding theme settings for Zurb.

--- theme-settings.php
+++ theme-settings.php
@@ -8,5 +8,23 @@
  * @param $form_state
  *   The form state.
  */
-function zurb_foundation_theme_form_system_theme_settings_alter(&$form, &$form_state) {
+function zurb_foundation_form_system_theme_settings_alter(&$form, &$form_state) {
+  $form['theme_javascript'] = array(
+    '#type' => 'details',
+    '#title' => t('Javascript Files'),
+  );
+
+  $form['theme_javascript']['use_zepto'] = array(
+    '#type' => 'checkbox',
+    '#title' => t('Include Zepto.js'),
+    '#description' => t('Foundation can leverage Zepto over jQuery - if you want to enable it .......'),
+    '#default_value' => theme_get_setting('use_zepto', 'zurb_foundation'),
+  );
+
+  $form['theme_javascript']['use_respondjs'] = array(
+    '#type' => 'checkbox',
+    '#title' => t('Implement Respond.js'),
+    '#description' => t('Foundation 4.x does not support IE8, but you can include Respond.js to add additional support.'),
+    '#default_value' => theme_get_setting('use_respondjs', 'zurb_foundation'),
+  );
 }
